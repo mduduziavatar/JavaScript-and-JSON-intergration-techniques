b0e36947478460638331b28cb0ac5ed07882f845
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2021-04-13 15:39:25 +0200

    added jQuery

--- arrays.js
+++ arrays.js
@@ -1,14 +1,17 @@
-function dataHandler(info) {
-    var output = "";
-    // to find items in an array you need to loop using a for loop that looks at the total length of items 
-    for (var i = 0; i <= info.links.length; i++) {
-        for (key in info.links[i]) {
-            if (info.links[i].hasOwnProperty(key)) {
-                output += '<li>' + '<a href = "' + info.links[i][key] + '">' + key + '</a>' + '</li>';
-            }// hasOwnProperty check
-        } // for each object
-    }// for each array in element
-    let updated = document.getElementById("link");
-    updated.innerHTML = output;
-}
+$(document).ready(function () {
+
+    $.getJSON('data.json', function (info) {
+        var output = "";
+        // to find items in an array you need to loop using a for loop that looks at the total length of items 
+        for (var i = 0; i <= info.links.length; i++) {
+            for (key in info.links[i]) {
+                if (info.links[i].hasOwnProperty(key)) {
+                    output += '<li>' + '<a href = "' + info.links[i][key] + '">' + key + '</a>' + '</li>';
+                }// hasOwnProperty check
+            } // for each object
+        }// for each array in element
+        let updated = document.getElementById("link");
+        updated.innerHTML = output;
+    })
+});
 
